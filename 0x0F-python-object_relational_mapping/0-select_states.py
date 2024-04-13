a6ddccb14c44444e9261277d0ef19476c6a1434a
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2024-04-14 01:25:43 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
+'''
+    main function to list all states
+'''
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
     
