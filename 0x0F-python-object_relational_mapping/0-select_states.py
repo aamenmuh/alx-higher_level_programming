272993d98a7460e14a40b3c9b921b927df4d16b4
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 03:09:27 +0200

    ss

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
     main function to list all states
 """
+import sys
+import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
     cur = db.cursor()
