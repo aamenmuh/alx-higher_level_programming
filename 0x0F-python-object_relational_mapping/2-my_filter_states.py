5271da306dbe9abeea7e7fa1cd7cc570f1478ba1
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 03:22:49 +0200

    ss

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute(f"SELECT * FROM states \
-            WHERE CONVERT(`name` USING Latin1) \
-            COLLATE Latin1_General_CS = {argv[4]} ORDER BY `id` ASC;"
+            WHERE `name` = '{argv[4]}' ORDER BY `id` ASC;"
     states = cur.fetchall()
 
     for state in states:
