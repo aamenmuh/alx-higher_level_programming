ba7681071ff9cbcd31245cc14e937fbce0728f82
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 03:49:24 +0200

    ss

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
             JOIN states ON cities.state_id = states.id \
-            WHERE CONVERT( `states.name` USING Latin1) \
-            COLLATE Latin1_General_CS = %s \
+            WHERE `states.name` = %s \
             ORDER BY `id` ASC;", (sys.argv[4],))
     states = cur.fetchall()
 
