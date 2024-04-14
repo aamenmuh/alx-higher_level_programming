62c443034018f6faaa928c50c81bafe213d660cc
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 04:02:09 +0200

    ss

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,11 +10,14 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    cur.execute("SELECT cities.name FROM cities \
             JOIN states ON cities.state_id = states.id \
-            WHERE states.name = h \
-            ORDER BY cities.id ASC;")
+            WHERE states.name = %s \
+            ORDER BY cities.id ASC;", (sys.argv[4],))
     states = cur.fetchall()
 
     for state in states:
-        print(state)
+        if (state != states[states.size()]):
+            print(state, ", ")
+        else
+            print(state, "")
