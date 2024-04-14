e774b65724feb71d5e211e6f823cfd94cd954b01
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 03:42:19 +0200

    ss

---
+++ 0x0F-python-object_relational_mapping/4-citiies_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Main function to filter states by name
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities \
+            ORDER BY `id` ASC;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
