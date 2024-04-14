8166c8692516932930176304c86239819f277fdb
Aamen Muharram <amenmuhdev@gmail.com>
2024-04-14 03:01:50 +0200

    ss

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if cycle exists
+ * @list: pointer to list
+ *
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *singleStep = list;
+    listint_t *doubleStep = list;
+
+   if (!list)
+       return (0);
+
+   while (singleStep && doubleStep && doubleStep->next)
+   {
+        singleStep = singleStep->next;
+        doubleStep = doubleStep->next->next;
+        if (singleStep == doubleStep)
+        {
+            return (1);
+        }
+   }
+
+   return (1);
+}
